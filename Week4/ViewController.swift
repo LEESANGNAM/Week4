cdfa27d901071cada5b0be463689d71c65ba1a47
LEESANGNAM <fgh0116@naver.com>
2023-08-08 12:01:51 +0900

    영화진흥위원회 API

--- Week4/ViewController.swift
+++ Week4/ViewController.swift
@@ -9,10 +9,25 @@ import UIKit
 import Alamofire
 import SwiftyJSON
 
+struct Movie{
+    var movieTitle: String
+    var release: String
+}
+
+
+
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var movieTableView: UITableView!
+    
+    var movieList: [Movie] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        movieTableView.delegate = self
+        movieTableView.dataSource = self
+        movieTableView.rowHeight = 60
         callRequest()
         // Do any additional setup after loading the view.
     }
@@ -25,10 +40,25 @@ class ViewController: UIViewController {
                 let json = JSON(value)
                 print("JSON: \(json)")
                 
-                let name = json["boxOfficeResult"]["dailyBoxOfficeList"][0]["movieNm"].stringValue
-                print("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ")
-                print(name)
+//                let name1 = json["boxOfficeResult"]["dailyBoxOfficeList"][0]["movieNm"].stringValue
+//                let name2 = json["boxOfficeResult"]["dailyBoxOfficeList"][1]["movieNm"].stringValue
+//                let name3 = json["boxOfficeResult"]["dailyBoxOfficeList"][2]["movieNm"].stringValue
+//
+//                self.movieList.append(contentsOf: [name1,name2,name3])
                 
+                
+                for item in json["boxOfficeResult"]["dailyBoxOfficeList"].arrayValue {
+                    let title = item["movieNm"].stringValue
+                    let release = item["openDt"].stringValue
+                    let movie = Movie(movieTitle: title, release: release)
+                    self.movieList.append(movie)
+                }
+                
+                
+                
+                
+                
+                self.movieTableView.reloadData()
             case .failure(let error):
                 print(error)
             }
@@ -38,3 +68,22 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController : UITableViewDelegate, UITableViewDataSource{
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movieList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = movieTableView.dequeueReusableCell(withIdentifier: "movieCell")!
+        cell.textLabel?.text = movieList[indexPath.row].movieTitle
+        cell.detailTextLabel?.text = movieList[indexPath.row].release
+        
+        return cell
+    }
+    
+
+    
+    
+}
