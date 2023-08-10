b47ddf1cd9d4089bff8a8ad41979a431ec430372
LEESANGNAM <fgh0116@naver.com>
2023-08-11 00:46:46 +0900

    언어 선택번역 추가

--- Week4/PapagoViewController.swift
+++ Week4/PapagoViewController.swift
@@ -9,6 +9,54 @@ import UIKit
 import Alamofire
 import SwiftyJSON
 
+enum Language: Int, CaseIterable{
+    case ko,ja,zh_CN,zh_TW,hi,en,es,fr,de,pt,vi,id,fa,ar,mm,th,re,it,unk
+    
+    var lang: String{
+        switch self{
+        case .ko:
+            return "한국어"
+        case .ja:
+            return "일본어"
+        case .zh_CN:
+            return "중국어간체"
+        case .zh_TW:
+            return "중국어번체"
+        case .hi:
+            return "힌디어"
+        case .en:
+            return "영어"
+        case .es:
+            return "스페인어"
+        case .fr:
+            return "프랑스어"
+        case .de:
+            return "독일어"
+        case .pt:
+            return "포르투갈어"
+        case .vi:
+            return "베트남어"
+        case .id:
+            return "인도네시아어"
+        case .fa:
+            return "페르시아어"
+        case .ar:
+            return "아랍어"
+        case .mm:
+            return "미얀마어"
+        case .th:
+            return "태국어"
+        case .re:
+            return "러시아어"
+        case .it:
+            return "이탈리아어"
+        case .unk:
+            return ""
+        }
+    }
+}
+
+
 
 class PapagoViewController: UIViewController {
     @IBOutlet weak var originalTextView: UITextView!
@@ -17,9 +65,17 @@ class PapagoViewController: UIViewController {
     
     @IBOutlet weak var requstButton: UIButton!
     
+    @IBOutlet weak var selectButton: UIButton!
+    @IBOutlet weak var selectButtonText: UITextField!
+    
+    let pickerView = UIPickerView()
+    
+    let langList = Language.allCases
+    
     let translateUrlEndPorint = "n2mt"
     let LanguageURL = "detectLangs"
-    
+    var beforeLang = ""
+    var afterLang = ""
     let header: HTTPHeaders = [
         "X-Naver-Client-Id": APIKey.naverCilentId,
         "X-Naver-Client-Secret": APIKey.naverCilentPW
@@ -27,28 +83,28 @@ class PapagoViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        originalTextView.text = ""
-        translateTextView.text = ""
-        translateTextView.isEditable = false
-        requstButton.setTitle("번역하기!!!!!", for: .normal)
-
+        setUpUI()
+        setupPickerView()
         // Do any additional setup after loading the view.
     }
     
-
+    func setUpUI(){
+        originalTextView.text = ""
+        translateTextView.text = ""
+        translateTextView.isEditable = false
+        
+        requstButton.setTitle("한국어 번역", for: .normal)
+        selectButton.setTitle("다른언어 선택", for: .normal)
+    }
+    
+    
+    
     @IBAction func requestButtonTapped(_ sender: UIButton) {
-        
-       
         cllRequestLanguage(endPointText: LanguageURL)
-        
-        
-       
-        
-        
     }
 }
 
-
+// MARK: - API
 extension PapagoViewController {
    
     func cllRequestLanguage(endPointText: String){
@@ -72,7 +128,7 @@ extension PapagoViewController {
         }
     }
     
-    func callRequest(endPointText: String, source: String, target: String = "ja"){
+    func callRequest(endPointText: String, source: String, target: String = "ko"){
         let parameters: Parameters = [
             "source": source,
             "target": target,
@@ -95,4 +151,58 @@ extension PapagoViewController {
         }
         
     }
+}
+// MARK: - picker
+extension PapagoViewController: UIPickerViewDelegate,UIPickerViewDataSource{
+    
+    
+    func setupPickerView(){
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        selectButtonText.inputView = pickerView
+        selectButtonText.tintColor = .clear
+    }
+    
+    //돌아가는거 몇개
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    // 셀 몇개
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return langList.count - 1
+    }
+    
+    // 선택
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let code = "\(langList[row])"
+        if component == 0{
+            beforeLang = code
+        }else{
+            afterLang = code
+        }
+        print("---------------")
+        print(beforeLang)
+        print(afterLang)
+        
+        
+        if !beforeLang.isEmpty && !afterLang.isEmpty{
+            callRequest(endPointText: translateUrlEndPorint, source: beforeLang,target: afterLang)
+            beforeLang = ""
+            afterLang = ""
+            view.endEditing(true)
+        }
+        
+    }
+    
+    
+    //
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return langList[row].lang
+    }
+    
+
+    
+
+    
+    
 }
